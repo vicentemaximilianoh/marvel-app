3de59cfdfacd31d7957af3b041f870d663aaab52
Maximiliano Vicente <vicente.maximilianoh@gmail.com>
2020-05-04 11:32:56 -0300

    Updates

---
+++ precache-manifest.feafbfd3f403bbeb5c08995d0c65ce16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4e6140277b9197722e80ae8ef1ee94",
+    "url": "/marvel-app/index.html"
+  },
+  {
+    "revision": "b8c9bcdec02fc17a3688",
+    "url": "/marvel-app/static/css/main.a973c036.chunk.css"
+  },
+  {
+    "revision": "a56aaa94be7e27b87129",
+    "url": "/marvel-app/static/js/2.88393a6f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/marvel-app/static/js/2.88393a6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c9bcdec02fc17a3688",
+    "url": "/marvel-app/static/js/main.31fb119c.chunk.js"
+  },
+  {
+    "revision": "46a58a630950e4617e1d",
+    "url": "/marvel-app/static/js/runtime-main.880d5167.js"
+  }
+]);
